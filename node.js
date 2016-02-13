d9d8ad1252dd8ea62a5d1c1d7fef0c6afd584100
unknown <diego.voltz@INFRA04.redehost.local>
2016-02-13 16:14:37 -0200

    ola14

--- node.js
+++ node.js
@@ -1,22 +1,61 @@
-// (c) http://www.obomprogramador.com
-// REST / JSON WS implementation with Node.js calling Java
+// blocker.js
+/*
+   This code is a bad example! It will
+   block the server. It is for demonstration
+   only.
+*/
 
-var restify = require('restify')
-var server = restify.createServer()
-var fs = require('fs')
-var java = require('java')
+var restify = require('restify');
+var fs = require('fs');
 
-// Página da aplicação: 
-function home(req,res,next) {
-   fs.createReadStream('./index.html', {autoClose: true}).pipe(res)
-}
 
+var server = restify.createServer();
+var fibonacci = function (n) {
+  if (n < 2)
+    return 1;
+  else
+    return fibonacci(n-2) + fibonacci(n-1);
+};
 
 // Home page:
-server.get('/mb',home)
+server.get('/',function(req, res) {
+   var bodyHtml = '<!DOCTYPE html><html><head><title>'
+                + 'Teste Node.js - O Bom Programador</title></head>'
+                + '<body>'
+		+ '<br/>Ok, agora, <a href="/blocker">o blocker</a>';
+                
+
+   bodyHtml += '</code></pre></body></html>';
+   res.writeHead(200, {
+     'Content-Length': Buffer.byteLength(bodyHtml),
+     'Content-Type': 'text/html'
+   });
+   res.write(bodyHtml);
+   res.end();      
+});
+
+// Blocker page:
+server.get('/blocker',function(req, res) {
+   var inicio = new Date();
+   var bodyHtml = '<!DOCTYPE html><html><head><title>'
+                + 'Teste Node.js - O Bom Programador</title></head>'
+                + '<body>';
+                
+   var resultado = fibonacci(45);
+   var fim = new Date();
+   bodyHtml += '<br/>' + resultado;
+   bodyHtml += '<br/>inicio: ' + inicio + ' fim: ' + fim;
+   bodyHtml += '</code></pre></body></html>';
+   res.writeHead(200, {
+     'Content-Length': Buffer.byteLength(bodyHtml),
+     'Content-Type': 'text/html'
+   });
+   res.write(bodyHtml);
+   res.end();      
+});
 
 
 // Start server
 server.listen(800, function() {
-  console.log('%s listening at %s', server.name, server.url);
+  console.log('Online: 800');
 });
