cb3d9c3c01ea000f39e56b0646bcbde29b71334c
unknown <diego.voltz@INFRA04.redehost.local>
2016-02-13 15:41:14 -0200

    ola6

--- node.js
+++ node.js
@@ -1,30 +1,30 @@
-function CalculadoraFrete(origem,destino,peso){
- var _distancia = googleMaps.getDistance(origem,destino);
- var _valorFrete=[];
- var _repositorio = new RepositorioTaxasFrete();
- function buscaMenorFrete(){
-  if(peso <= 30){
-   if(_distancia < 50){
-    _valorFrete.push(_repositorio.taxaMotoFreteMunicipal());
-   }else{
-    _valorFrete.push(_repositorio.taxaCorreios());
-   }
-  }
-  else if(peso>30 && peso<800){  
-   if(_distancia < 140){
-    _valorFrete.push(_repositorio.taxaCarro());
-   }else{
-    _valorFrete.push(_repositorio.taxaCaminhao());
-   }
-  }
-  else {
-   if(_distancia < 300){
-    _valorFrete.push(_repositorio.taxaCaminhao());
-   }else{
-    _valorFrete.push(_repositorio.taxaCarreta());
-   }
-  }
-  _valorFrete.sort();
-  return _valorFrete[0];
- }
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+ 
+var app = express();
+ 
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+ 
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
 }
+ 
+app.get('/', routes.index);
+app.get('/users', user.list);
+ 
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
