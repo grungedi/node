85e890a380c4af7b1f7bf353c6726a72bf8aa828
unknown <diego.voltz@INFRA04.redehost.local>
2016-02-13 16:05:41 -0200

    ola10

--- node.js
+++ node.js
@@ -1,63 +1,17 @@
-var express = require('express')
-  , app = express();
-
-app.configure(function(){
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-});
-
-var clientes = [];
-
-app.get('/', function(req, res){
-  var html  = '<html><body>';
-  html += '<form action="/cliente" method="post">';
-  html += '<label>Nome: <input type="text" name="cliente[nome]"></label><br>';
-  html += '<label>Idade: <input type="text" name="cliente[idade]"></label><br>';
-  html += '<button type="submit">Enviar</button>';
-  html += '</form>';
-  html += '<br>';
-  html += '<h1>Lista de clientes</h1>';
-  html += '<ul>';
-  for(var i = 0; i < clientes.length; i++){
-    html += '<li>'+clientes[i].nome+' | '+clientes[i].idade;
-    html += '<a href="/cliente/'+i+'/editar">Editar</a> | ';
-    html += '<a href="/cliente/'+i+'">Excluir</a></li>';
-  }
-  html += '</ul></body></html>';
-  res.send(html);
-});
-
-app.post('/cliente', function(req, res){
-  var cliente = req.body.cliente;
-  clientes.push(cliente);
-  res.redirect('/');
-});
-
-app.get('/cliente/:id/editar', function(req, res){
-  var id = req.params.id;
-  var html  = '<html><body>';
-  html += '<h1>Editar dados do cliente: '+clientes[id].nome+'</h1>';
-  html += '<form action="/cliente/'+ id +'" method="post">';
-  html += '<input type="hidden" name="_method" value="put">'; // Força o formulário realizar um comando PUT no submit.
-
-  html += '<label>Nome: <input type="text" name="cliente[nome]" value="'+clientes[id].nome+'"></label>';
-  html += '<label>Idade: <input type="text" name="cliente[idade]" value="'+clientes[id].idade+'"></label>';
-  html += '<button type="submit">Enviar</button>';
-  html += '</form>';
-  html += '</html>';
-  res.send(html);
-});
-
-app.put('/cliente/:id', function(req, res){
-  var id = req.params.id;
-  clientes[id] = req.body.cliente;
-  res.redirect('/');
-});
-
-app.del('/cliente/:id', function(req, res){
-  var id = req.params.id;
-  clientes.splice(id, 1);
-  res.redirect('/');
-});
-
-app.listen(800);
+var frase;
+ 
+carregaFrase = function (callback) {  
+  setTimeout(function() {
+    //Simula leitura da frase no banco de dados.
+    frase = "Minha frase obstrutiva";
+    callback();
+  }, 3000)
+}
+ 
+imprimeFrase = function () {
+  console.log(frase);
+}
+ 
+carregaFrase(imprimeFrase);
+ 
+console.log(“Olá");
