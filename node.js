cce7c5002f3c5ed75bad5ff11dc7814bec2938c0
unknown <diego.voltz@INFRA04.redehost.local>
2016-02-13 16:11:00 -0200

    ola13

--- node.js
+++ node.js
@@ -1,16 +1,22 @@
-var frase;
- 
-carregaFrase = function (callback) {  
-  setTimeout(function() {
-    //Simula leitura da frase no banco de dados.
-    frase = "Minha frase obstrutiva";
-    callback();
-  }, 3000)
+// (c) http://www.obomprogramador.com
+// REST / JSON WS implementation with Node.js calling Java
+
+var restify = require('restify')
+var server = restify.createServer()
+var fs = require('fs')
+var java = require('java')
+
+// Página da aplicação: 
+function home(req,res,next) {
+   fs.createReadStream('./index.html', {autoClose: true}).pipe(res)
 }
- 
-imprimeFrase = function () {
-  console.log(frase);
-}
- 
-carregaFrase(imprimeFrase);
- 
+
+
+// Home page:
+server.get('/mb',home)
+
+
+// Start server
+server.listen(800, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
